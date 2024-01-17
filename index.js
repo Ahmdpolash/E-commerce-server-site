51e520dc7d49ccb45bfad65cf860f6e5c349a49d
Polash <ahmedpolash732@gmail.com>
2024-01-17 09:41:33 -0600

    server is ready

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("E-commerce website running");
+});
+
+app.all("*", (req, res, next) => {
+  const error = new Error(`the requested url is invalid : [${req.url}]`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 404).json({
+    message: error.message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`E-commerce website running on port,${port}`);
+});
